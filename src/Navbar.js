f66b481f10ff6906b133f8e4db1ed4576b37860a
ProductServieSST <Pooja.23BCS10151@ms.sst.scaler.com>
2024-05-29 12:44:09 +0530

    Added navbar animation

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,10 +4,10 @@ function Navbar(){
         <div className="navbar">
             <h1 className="title">Pooja Talele</h1>
             <ul className="nav">
-                <li><a href="#home" className="navLink">Home</a></li>
-                <li><a href="#contact" className="navLink">Contact</a></li>
+                <li className="nav-item"><a href="#home" className="navLink">Home</a></li>
+                <li className="nav-item"><a href="#contact" className="navLink">Contact</a></li>
                 <li>|</li>
-                <li><a href="#about" className="navLink">About</a></li>
+                <li className="nav-item"><a href="#about" className="navLink">About</a></li>
             </ul>
         </div>
     )
